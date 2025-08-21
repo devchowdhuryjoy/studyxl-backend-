7bc36e6c17f14ee5195f4bdfd9332b1f72c6b1a4
devchowdhuryjoy <chowdhuryimran262@gmail.com>
2025-08-21 14:01:33 +0600

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Study XL</title>
+
+    <!-- Bootstrap CSS -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+    >
+  </head>
+  <body class="d-flex align-items-center justify-content-center vh-100 bg-dark">
+    <h1 class="text-white text-center fw-bold display-4">
+      Welcome to Study XL
+    </h1>
+
+    <!-- Bootstrap JS (optional) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
+
